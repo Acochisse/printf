79050cf20c68c8c05b85a1ee7abb7d098aa005f2
Scott Seufert <scott.seufert@gmail.com>
2020-07-18 20:35:14 +0000

    Added _printf

---
+++ _printf.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+#include <stdlib.h>
+#include <stdarg.h>
+#include <stdio.h>
+
+int _printf(const char *format, ...)
+{
+	va_list ap;
+	int i, j, count = 0;
+	get_f conv_spec[] = {
+		{'c', print_char}, {NULL, NULL} };
+
+	if (!format || !_strcmp(format, "%"))
+	{
+		return (99);
+	}
+
+	va_start(ap, format);
+	for (i = 0; format[i]; i++, count++)
+	{
+		for (j = 0; conv_spec[j].s_char; j++)
+		{
+			if (format[i + 1] == conv_spec[j].s_char)
+			{
+				count += conv_spec[j].func(ap);
+				i++;
+				break;
+			}
+		}
+	}
+
+	va_end(ap);
+	return (count);
+}
---
+++ print_char.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * print_char - Print char
+ * @ap: arg ptr
+ * Return: char
+ */
+
+int print_char(va_list ap)
+{
+	int c = va_arg(ap, int);
+
+	_putchar(c);
+	return (0);
+}
