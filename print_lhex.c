74306df6fb1e460161ed7491bfc0e56490c4b4c4
Acochisse <lee.stewart909@gmail.com>
2020-07-20 19:22:12 -0500

    created function print_lhex from _convertlower

---
+++ print_lhex.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * print_hex - converts input into codeasaurus hex
+ * @ap: arg pointer
+ * Return: 0
+ */
+
+int print_lhex(va_list ap)
+
+{
+	unsigned int i = va_arg(ap, unsigned int);
+
+	_puts(_convertlower(i, 16));
+	return (0);
+}
